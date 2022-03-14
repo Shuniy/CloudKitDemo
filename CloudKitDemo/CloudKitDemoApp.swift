459b863a78e9b33535e83bb7e83fd765652390de
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-03-14 15:01:29 +0530

    Initial commit

---
+++ CloudKitDemo/CloudKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitDemoApp.swift
+//  CloudKitDemo
+//
+//  Created by Shubham Kumar on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
